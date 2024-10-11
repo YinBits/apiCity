d57cd268f9599cde6d5decaf7e51fae6a4a58ad2
Bruno Marçal dos Santos <12428396@FAJ2.ACAD.LOCAL>
2024-10-11 19:40:44 -0300

    Atualização

--- src/server.js
+++ src/server.js
@@ -11,20 +11,20 @@ app.use(cors());
 app.use(bodyParser.json());
 
 let sensorLumAtual = null; 
-let semafaroUmAtivo = 0; // Usando 0 para inativo
-let semafaroDoisAtivo = 0; // Usando 0 para inativo
+let semafaroUmAtivo = 0;
+let semafaroDoisAtivo = 0; 
 
-// Rota única para controlar semáforos e iluminação
+
 app.route('/')
     .post((req, res) => {
         const { DSU, DSD, SL } = req.body;
 
-        // Atualiza o valor do sensor de luminosidade
+       
         sensorLumAtual = SL;
 
         // Lógica de controle dos semáforos
-        semafaroUmAtivo = DSU < DSD ? 1 : 0; // Semáforo um ativo se a distância for menor
-        semafaroDoisAtivo = semafaroUmAtivo ? 0 : 1; // Semáforo dois ativo se o um estiver inativo
+        semafaroUmAtivo = DSU < DSD ? 1 : 0; 
+        semafaroDoisAtivo = semafaroUmAtivo ? 0 : 1;
 
         // Estado das luzes baseado no sensor de iluminação
         const estadoLuzes = lightsOfCity(SL);
