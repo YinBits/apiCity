62677e20b311235a23271ecdab60665420d8b458
Bruno Marçal dos Santos <12428396@FAJ2.ACAD.LOCAL>
2024-10-21 20:16:36 -0300

    Sistema de Semafaro

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0'; // Escuta em todas as interfaces
 
 // Middleware
 app.use(cors());
@@ -14,44 +15,43 @@ let sensorLumAtual = null;
 let semafaroUmAtivo = 0;
 let semafaroDoisAtivo = 0; 
 
+let timestampAnterior = null;
+let timestampAtual = null;
+let timestampProximo = null;
 
 app.route('/')
     .post((req, res) => {
-        const { DSU, DSD, SL } = req.body;
+        const {SL} = req.body;
 
-       
         sensorLumAtual = SL;
-
-        // Lógica de controle dos semáforos
-        semafaroUmAtivo = DSU < DSD ? 1 : 0; 
-        semafaroDoisAtivo = semafaroUmAtivo ? 0 : 1;
-
-        // Estado das luzes baseado no sensor de iluminação
-        const estadoLuzes = lightsOfCity(SL);
         
-        const resposta = {
-            SU: semafaroUmAtivo,
-            SD: semafaroDoisAtivo,
-            SL,
-            IL: estadoLuzes,
-        };
+        const estadoLuzes = lightsOfCity(SL);
+       
+        const agora = Date.now(); 
 
-        res.status(200).json(resposta);
+        if (timestampAtual === null) {
+            timestampAtual = agora;
+            timestampProximo = agora + 10000; // 10 segundos no futuro
+        }
+        if (agora >= timestampProximo) {
+            semafaroUmAtivo = semafaroUmAtivo === 0 ? 1 : 0  
+            semafaroDoisAtivo = semafaroUmAtivo === 1 ? 0 : 1;
+            
+            console.log(`Semáforo Um: ${semafaroUmAtivo}, Semáforo Dois: ${semafaroDoisAtivo}`);
+
+            timestampAnterior = timestampAtual;
+            timestampAtual = agora;
+            timestampProximo = agora + 10000; // Atualiza para 10 segundos depois
+        }
+
+      
+        
+        // Formata a resposta como uma string
+        const resposta = `${estadoLuzes}${semafaroUmAtivo}${semafaroDoisAtivo}`;
+
+        res.status(200).send(resposta); // Envia a resposta como string
     })
-    .get((req, res) => {
-        // Usa o valor atual do sensorLum
-        const estadoLuzes = sensorLumAtual !== null ? lightsOfCity(sensorLumAtual) : 0;
 
-        const resposta = {
-            sensorLum: sensorLumAtual,
-            IL: estadoLuzes,
-            SU: semafaroUmAtivo,
-            SD: semafaroDoisAtivo,
-        };
-
-        res.status(200).json(resposta);
-    });
-
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+app.listen(port, host, () => {
+    console.log(`Servidor rodando em http://${host}:${port}`);
 });
